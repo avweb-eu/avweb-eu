38e0044a930b436d1b77c4854dccece427514135
avweb-eu <80524368+avweb-eu@users.noreply.github.com>
2021-03-12 16:51:21 +0100

    Add files via upload

---
+++ zacky-installer.datarecord.php
@@ -0,0 +1,18 @@
+<?php /*
+
+This file contains internal information for the Zacky Installer.
+Do NOT change. Removing it might prevent you from using the installer 
+on this installation in the future.
+
+---
+QlpoOTFBWSZTWfrR0PYAAJlfgEAQQId/8CHwDRC/79+7MAEZrWw1J6UzU8p6jYUA/VGj1ABpoNGh
+E0m0U8Em1G1NNDIAABieoNT0iYjRPESek2ghhogNqG1CfVrc7CH9nKpAcpXYvp/dzMTQCXkgoSBP
+b4xaoaemGNoYCtK5UZYWDWbNiLwFQGFYtL5RBmHCoY8brAK3rqQ00jhChQOARC0AgSGZsqoj0TFN
+JUoa4jsXO9ymAPO/QxjGu36xumMiSCQURs7unDJYHN/gsVLVVqo0+ucfdYsZJoE6LBva+v0EvW9F
+wiQ6EVtDYHSZxHyCBVteTaIapwPGGYiJXG1d1+e+m/QiOW9aoRyeIFBGTEkihFhscpoLtkr9iUd9
+8iNQkovqv3zV7BnyNO0wsGJIuEEVtXQkw5WZCquBQmitcwULzJ2U3atK3XzclIMYyAlbLd/F3JFO
+FCQ+tHQ9gA==
+
+...
+
+*/
